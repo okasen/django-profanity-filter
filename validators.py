c1fb89c866b301fd87e73f93d79ff3ce0c7c37c6
Jennifer Black <JenniLBlack@outlook.com>
2021-06-07 21:42:45 +0100

    adding a method to check for couched words

--- validators.py
+++ validators.py
@@ -7,3 +7,7 @@ pf = ProfanityFilter()
 def validate_is_profane(value):
     if pf.is_profane(value) is True:
         raise ValidationError('Please remove any profanity/swear words.')
+
+def validate_is_profane_nospace(value):
+    if pf.is_profane_nospace(value) is True:
+        raise ValidationError('Please remove any profanity/swear words.')
